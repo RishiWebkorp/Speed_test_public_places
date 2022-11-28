9df11e61f0fc1e484729cbd782d63bc06e97224e
RishiWebkorp <rishi@webkorps.com>
2022-11-29 01:14:09 +0530

    rendering list of places in react

--- app/javascript/components/places_list.jsx
+++ app/javascript/components/places_list.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from "react-dom/client";
 
+function PlacesList(){
+  const [loading, setLoading] = useState(true);
+  const [loadedPlaces, setloadedPlaces] = useState([]);
 
-class PlacesList extends React.Component {
-  render(){
-    return(
-      <div>Places list rendered in react </div>
-    )
+  useEffect(() => {
+    const apiEndpoint = "/api/places"
+    fetch(apiEndpoint)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setloadedPlaces(data["places"])
+        setLoading(false)
+      });
+  },[])
+
+  const loadingSection = (<div>Loading...</div>)
+    console.log(loadedPlaces)
+  const dataSection = loadedPlaces.map((place,index) =>
+      <div key={index}>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>City</th>
+            <th>Recent Upload Speed</th>
+            <th>Recent Upload Speed Units</th>
+            <th>Number of Measurements </th>
+          </tr>
+          <tr>
+            <td>{place.name}</td>
+            <td>{place.city}</td>
+            <td>{place.most_recent_download_speed}</td>
+            <td>{place.most_recent_download_units}</td>
+            <td>{place.number_of_measurements}</td>
+          </tr>
+        </table>
+      </div>
+  )
+  if(loading){
+    return loadingSection
+  }
+  else{
+    return dataSection
   }
 }
 
